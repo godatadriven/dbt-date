c6d42aa86ee9374d096daaebe6297dbaec2db1c6
Padraic Slattery <pgoslatara@gmail.com>
2026-03-27 16:31:11 +0100

    Make date() and datetime() macros dispatchable
    Wrap both macros with adapter.dispatch() so adapter-specific packages
    can override them (e.g. to handle timezone-naive timestamp types).
    Existing behaviour is preserved in default__date and default__datetime.
    
    Closes #47

--- macros/_utils/modules_datetime.sql
+++ macros/_utils/modules_datetime.sql
@@ -1,11 +1,19 @@
 {% macro date(year, month, day) %}
-    {{ return(modules.datetime.date(year, month, day)) }}
+  {{ return(adapter.dispatch("date", "dbt_date")(year, month, day)) }}
+{% endmacro %}
+
+{% macro default__date(year, month, day) %}
+  {{ return(modules.datetime.date(year, month, day)) }}
 {% endmacro %}
 
 {% macro datetime(
     year, month, day, hour=0, minute=0, second=0, microsecond=0, tz=None
 ) %}
-    {% set tz = tz if tz else var("dbt_date:time_zone") %}
+  {{ return(adapter.dispatch("datetime", "dbt_date")(year, month, day, hour, minute, second, microsecond, tz)) }}
+{% endmacro %}
+
+{% macro default__datetime(year, month, day, hour, minute, second, microsecond, tz) %}
+  {% set tz = tz if tz else var("dbt_date:time_zone") %}
     {{
         return(
             modules.datetime.datetime(
