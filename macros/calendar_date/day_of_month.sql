eed68d04a4b4b3c36cdd54b3fc1d37c7ec017972
Padraic Slattery <pgoslatara@gmail.com>
2026-05-18 10:23:37 +0200

    Make remaining calendar_date macros dispatchable
    Wraps 21 user-facing macros in adapter.dispatch() and moves the existing
    implementations into default__ variants. This follows the pattern from
    PRs #48 (date/datetime) and #50 (get_fiscal_periods) and lets adapter
    packages or end users override any of these macros without forking.
    
    Macros covered: day_of_month, last_month, last_month_name, last_month_number,
    last_week, n_days_ago, n_days_away, n_months_ago, n_months_away, n_weeks_ago,
    n_weeks_away, next_month, next_month_name, next_month_number, next_week, now,
    periods_since, round_timestamp, today, tomorrow, yesterday.
    
    Note: day_of_month previously defined a redshift__ override that was never
    reached because the macro was called directly. The dispatch wrapper activates
    that override on Redshift.

--- macros/calendar_date/day_of_month.sql
+++ macros/calendar_date/day_of_month.sql
@@ -1,4 +1,10 @@
-{%- macro day_of_month(date) -%} {{ dbt_date.date_part("day", date) }} {%- endmacro %}
+{%- macro day_of_month(date) -%}
+    {{ return(adapter.dispatch("day_of_month", "dbt_date")(date)) }}
+{%- endmacro %}
+
+{%- macro default__day_of_month(date) -%}
+    {{ dbt_date.date_part("day", date) }}
+{%- endmacro %}
 
 {%- macro redshift__day_of_month(date) -%}
     cast({{ dbt_date.date_part("day", date) }} as {{ dbt.type_bigint() }})
--- macros/calendar_date/next_week.sql
+++ macros/calendar_date/next_week.sql
@@ -1,1 +1,7 @@
-{%- macro next_week(tz=None) -%} {{ dbt_date.n_weeks_away(1, tz) }} {%- endmacro -%}
+{%- macro next_week(tz=None) -%}
+    {{ return(adapter.dispatch("next_week", "dbt_date")(tz)) }}
+{%- endmacro -%}
+
+{%- macro default__next_week(tz=None) -%}
+    {{ dbt_date.n_weeks_away(1, tz) }}
+{%- endmacro -%}
